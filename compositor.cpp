3bd29ec4cfa1a076d16e801c6c443c0f72117f8d
papamitra <papamitra@gmail.com>
2015-04-19 09:47:58 +0900

    Create compositor class

--- compositor.cpp
+++ compositor.cpp
@@ -1,6 +1,5 @@
 
-#include <wayland-server.h>
-#include <wayland-server-protocol.h>
+#include "compositor.hpp"
 
 #include <cassert>
 #include <cstdio>
@@ -14,41 +13,43 @@ struct surface {
     wl_resource* resource;
 };
 
-static void
-compositor_create_surface(struct wl_client *client,
-			  struct wl_resource *resource, uint32_t id)
+static const struct wl_surface_interface surface_interface = {};
+
+namespace {
+
+void handle_create_surface(
+    struct wl_client *client,
+    struct wl_resource *resource, uint32_t id)
 {
     std::clog << __PRETTY_FUNCTION__ << std::endl;
-#if 0
-//	struct weston_compositor *ec = wl_resource_get_user_data(resource);
-	surface *surf = new surface{};
 
-	if (surf == NULL) {
+	compositor *comp = static_cast<compositor*>(wl_resource_get_user_data(resource));
+	surface *sf = new surface{};
+
+	if (!sf) {
 		wl_resource_post_no_memory(resource);
 		return;
 	}
 
-	surf->resource =
+	sf->resource =
 		wl_resource_create(client, &wl_surface_interface,
 				   wl_resource_get_version(resource), id);
-	if (surface->resource == NULL) {
-		weston_surface_destroy(surface);
-		wl_resource_post_no_memory(resource);
-		return;
-	}
-	wl_resource_set_implementation(surface->resource, &surface_interface,
-				       surface, destroy_surface);
-#endif
+	if (!sf->resource) {
+        delete sf;
+        wl_resource_post_no_memory(resource);
+        return;
+    }
+    wl_resource_set_implementation(sf->resource, &surface_interface,
+                                   sf, nullptr /*TODO: destroy_surface*/);
 }
 
 static const struct wl_compositor_interface compositor_interface = {
-    compositor_create_surface,
+    handle_create_surface,
     NULL
 };
 
-static void
-compositor_bind(struct wl_client *client, void *data,
-                uint32_t ver, uint32_t id)
+void handle_bind(struct wl_client *client, void *data,
+                 uint32_t ver, uint32_t id)
 {
     printf("compositor_bind\n");
 
@@ -61,21 +62,14 @@ compositor_bind(struct wl_client *client, void *data,
     wl_resource_set_implementation(res, &compositor_interface, data, NULL);
 }
 
-} // namespace yawc
-
-int main(void) {
-
-    wl_display* d = wl_display_create();
-
-    wl_global* const g = wl_global_create(d, &wl_compositor_interface,
-                                          1, d, yawc::compositor_bind);
-    assert(g);
-
-    const int stat = wl_display_add_socket(d, "wayland-0");
-    assert(stat == 0);
-
-    wl_display_run(d);
-
-    return 0;
 }
 
+void compositor::global_create(wl_display* disp) {
+    static compositor comp{disp};
+
+    wl_global* const g = wl_global_create(disp, &wl_compositor_interface,
+                                          1, &comp, handle_bind);
+    assert(g);
+}
+
+} // namespace yawc
